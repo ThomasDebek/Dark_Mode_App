0e89c8105230cbf2c7b6a1194021e40f91aa2d7d
ThomasDebek <tom.debek@gmail.com>
2025-12-01 22:15:09 +0100

    db: set defult to false

--- db/migrate/20251201211345_add_dark_mode_to_users.rb
+++ db/migrate/20251201211345_add_dark_mode_to_users.rb
@@ -1,5 +1,5 @@
 class AddDarkModeToUsers < ActiveRecord::Migration[8.0]
   def change
-    add_column :users, :dark_mode, :boolean
+    add_column :users, :dark_mode, :boolean, default: false
   end
 end
