26a319d29b6524393c16eefa2d1b09cb92b3789c
ThomasDebek <tom.debek@gmail.com>
2025-12-01 22:27:31 +0100

    generate new controller - dark mode

---
+++ app/controllers/dark_mode_controller.rb
@@ -0,0 +1,8 @@
+class DarkModeController < ApplicationController
+
+  def create
+    Current.user.update(dark_mode: !Current.user.dark_mode)
+    redirect_to root_path
+  end
+
+end
--- config/routes.rb
+++ config/routes.rb
@@ -2,6 +2,7 @@ Rails.application.routes.draw do
   resource :registrations, only: [:new, :create]
   resource :session
   resources :passwords, param: :token
+  resources :dark_mode, only: [:create], controller: :dark_mode
   root 'pages#home'
 
 end
